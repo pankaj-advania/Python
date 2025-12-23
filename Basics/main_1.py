b94a9c4f0ec3c82a00ca9214f188e640137aa5fb
pankaj-advania <advaniapankaj@gmail.com>
2025-12-24 02:20:50 +0530

    learing print function, variables, keywords, datatypes, and literals

--- Basics/main_1.py
+++ Basics/main_1.py
@@ -1,1 +1,147 @@
-print("Hello World!")
+# # print("Hello World!")
+# # print("Hello World")
+# # print("Mehak Napa")
+# # print (7)
+# # print(7.7)
+# # print(True)
+# # print("Hello", 1, 4.5,True)
+# # print("Hello",1,4.5,True, sep='/')
+# # print("Hello",1,3.5,end='-')
+# # print("Duggu")
+
+
+# # # Intenger
+# # print(8)
+# # # 1*10^308
+# # print(1e308)
+
+
+# #  # Decimal/ Float
+# # print (8.55)
+# # print (1.7e309)
+
+
+# # # Boolean 
+# # print(True)
+# # print(False)
+
+
+# # # String
+# # print("Pankaj")
+
+
+# # # Comlex
+# # print(5+6j)
+
+# # # list -> c-> Array
+# # print([1,2,3,4,5])
+
+# # # Tuple 
+# # print({1,2,3,4,5})
+
+# # # # Dictionary
+# # # print({"Name" : "Pankaj","Age" : 21,"Address" : "Jallopur"})
+
+
+# # # # Type 
+# # # type (4)
+# # # type (True)
+# # # type ("Pankaj")
+# # # print(type)
+
+
+# # # Variables
+# # # Static Vs Dynamic Typing
+# # # Static Vs Dynamic Blinding
+# # # Styish declaration techniques
+
+
+# # # name = "Pankaj"
+# # # print(name)
+# # # a = 5
+# # # b = 7
+
+# # print(a+ b)
+# # # Dynamoc Typing 
+# # # a = 4
+
+# # # #Static Typing 
+# # # int a = 4
+
+
+# # # Dynamic Blinding
+# # # a = 5 
+# # # print(a)
+# # # a = "Pankaj"
+# # # print(a)
+
+
+# # # Static Binding
+# # # int a =5
+# # # a = 1
+# # # b = 2
+# # # c = 3
+# # # print(a,b,c)
+# # # a,b,c = 1,2,3
+# # # print(a,b,c)
+
+# # # a=b=c = 5
+# # # print(a,b,c)
+
+# # # Comment
+
+# # # # Keywords & Indentifiers 
+# # # # Keywords
+# # # # Identifiers
+# # # # you can't start with a digit
+# # # name1 = "Pankaj"
+# # # print(name1)
+
+# # # # you can use special chars -> _
+# # # _ = "Pankaj"
+# # # print(_)
+
+# # # # identiers can not be keywords 
+# fnum = int(input("Enter your number"))
+# snum = int(input ("Enter your number"))
+# print(fnum, snum)
+# result = (fnum + snum)
+# print(result)
+# Literals 
+a = 0b1010 # binary literals
+b = 100 # Decimal literals 
+c = 0o310 # octal Literals 
+d = 0x12c # Hexadecimal Literals
+
+# Float Litrals 
+float_1 = 10.5 
+float_2 = 1.5e2
+float_3 =1.5e-3
+
+# Complex Literal
+x = 3.14j
+print (a,b,c,d)
+print(float_1,float_2,float_3)
+print(x, x.imag, x.real)
+
+string = "this is Pyhton"
+string = " this is Python "
+char = "c"
+mutiline_str = """this is a multiline string with more than oneline code ."""
+uncode = "\U0001F600\U0001F606\U0001F923"
+raw_str = r"raw \n string"
+print(string)
+print(char)
+print(raw_str)
+
+a = True + 4
+b = False + 10 
+
+print("a:", a)
+print("b:",b)
+
+ # none
+a = 21
+b = 45
+
+
---
+++ Basics/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+a = True + 4
+# b = False + 10 
+
+# print("a:", a)
+# print("b:",b)
+
+#  # none
+# k = none
+# a = 21
+# b = 45
