727d4a511ef94bb9d7bf427781e9a5967ae6f5c8
pankaj-advania <advaniapankaj@gmail.com>
2025-12-27 23:22:38 +0530

    loop nested loops

--- Basics/main_1.py
+++ Basics/main_1.py
@@ -260,10 +260,27 @@
 # for i in "pankaj" :
 #     print(i)
  
-curr_pop = 10000
+# curr_pop = 10000
 
 # for i in range (10,0,-1) :
 #     print (i,curr_pop)
-#     curr_pop = curr_pop - .1 * curr_pop
+#     curr_pop = curr_pop / 1.1
 
-print(.1 * 10000)c
+# code here 
+# n = int(input("enter n"))
+
+# result = 0 
+# fact = 1 
+
+# for i in range (1,n+1):
+#     fact = fact * i
+#     result = result + i/fact
+
+#     print(result)
+
+rows = int(input("enter the number :"))
+
+for i in range (1,rows+1):
+ for j in range (1,i+1):
+  print ("*",end=" ")
+ print()
--- Basics/tempCodeRunnerFile.py
+++ Basics/tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-number = number // 10
+curr_pop = 10000
+
+for i in range (10,0,-1) :
+    print (i,curr_pop)
+    curr_pop = curr_pop / 1.1
